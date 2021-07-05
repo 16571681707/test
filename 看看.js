00d9b6fd65fba52340bb21b7a2079e8ba59a8c48
16571681707 <77574453+16571681707@users.noreply.github.com>
2021-07-05 23:14:37 +0800

    Update 看看.js

--- 看看.js
+++ 看看.js
@@ -1,13 +1,15 @@
 var times = 0
 
+xuanfu()
+
 while(times < 35) {
     sleep(1500)
     times = times + 1
     向下滑动()
+    sleep(1800)
     back()
+    console.log("看第" + times + "篇")
     sleep(4000)
-    
-    console.log(times)
 }
 
 
@@ -20,3 +22,111 @@ function 向下滑动() {
     swipe(width / 2, height - 200, width / 2 - suiji, end, 7000)
     sleep(3000)
 }
+
+function xuanfu() {
+    var window = floaty.window(
+        <frame padding="1">
+            <vertical  >
+                <horizontal>
+                    <button id="action" text="点击" w="35" h="50" style="Widget.AppCompat.Button.Colored" textSize="12sp" />
+                    <button id="begin" text="日志" w="35" h="50" textSize="12sp" />
+                    <button id="end" text="结束" w="35" h="50" textSize="12sp" />
+                </horizontal>
+
+                <com.stardust.autojs.core.console.ConsoleView h="150" w="180" id="console" bg="#80000000" />
+
+
+            </vertical>
+        </frame>
+
+    );
+
+    window.console.setConsole(runtime.console);
+    window.setPosition(0, device.height / 5 * 4);
+    // window.console.findViewById(org.autojs.autojs.R.id.input_container).setVisibility(android.view.View.GONE);
+    window.console.attr("visibility", "gone");
+
+
+    //日志和结束按钮不显示
+    window.begin.setVisibility(8)
+    window.end.setVisibility(8)
+    // window.console.setTouchable(false);
+
+    var x = 0, y = 0;
+    var windowX, windowY;
+    var downTime;
+    window.action.setOnTouchListener(function (view, event) {
+        switch (event.getAction()) {
+            case event.ACTION_DOWN:
+                x = event.getRawX();
+                y = event.getRawY();
+                windowX = window.getX();
+                windowY = window.getY();
+                downTime = new Date().getTime();
+                return true;
+            case event.ACTION_MOVE:
+                //移动手指时调整悬浮窗位置
+                window.setPosition(windowX + (event.getRawX() - x),
+                    windowY + (event.getRawY() - y));
+                return true;
+            case event.ACTION_UP:
+                //手指弹起时如果偏移很小则判断为点击
+                if (Math.abs(event.getRawY() - y) < 5 && Math.abs(event.getRawX() - x) < 5) {
+                    onClick();
+                }
+                return true;
+        }
+        return true;
+    });
+
+    function onClick(str, sec) {
+
+        if (window.end.visibility == 8) {
+            window.console.setVisibility(0)
+            window.end.setVisibility(0)
+        } else {
+            window.console.setVisibility(8)
+            window.end.setVisibility(8)
+        }
+    }
+
+    var timerId;
+    function 显示状态信息(str, sec) {
+        try {
+            clearTimeout(timerId);
+        }
+        catch (error) {
+            toastLog("error");
+        }
+        ui.run(() => {
+            window.action.setText(str);
+        });
+        timerId = setTimeout(function () {
+            ui.run(() => {
+                window.action.setText("");
+            });
+        }, sec * 1000);
+    }
+
+
+    window.begin.click(() => {
+        window.begin.text() == "显示日志"
+        if (window.begin.text() == "显示日志") {
+            window.begin.setText("隐藏日志")
+            window.console.attr("visibility", "visible");
+        } else {
+            window.begin.setText("显示日志")
+            window.console.attr("visibility", "gone");
+        }
+    });
+
+
+
+    window.end.click(() => {
+        floaty.closeAll();
+        engines.stopAll();
+        exit();
+    });
+
+}
+//悬浮窗结束-----------
